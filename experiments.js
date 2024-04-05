0a617e82b294a138b535d014738be514a1605b35
Henry Martija <henrymartija@gmail.com>
2024-04-05 16:14:22 -0700

    📝 Delete experiments.js file and introduce webdev directory for HTML files and scripts.   ✨ Create experiments.js in webdev/html directory to modify elements dynamically.   ✨ Introduce newindex.html, newspaper.html, newspaperstyle.css, and     talking to servers files in webdev/html directory. Add HTML content and     styles for new features.   ✨ Add wordle.css and wordle.html in the talking to servers directory for the     Word Masters game with input boxes.

--- experiments.js
+++
@@ -1,5 +0,0 @@
-const elementsToChange = document.querySelectorAll(".js-target");
-for (let i = 0; i < elementsToChange.length; i++) {
-  const currentElement = elementsToChange[i];
-  currentElement.innerText = "Modified by JavaScript!";
-}
